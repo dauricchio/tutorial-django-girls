f74648348ded8bd2bf60f32e3112d5fc1b3e7321
dauricchio <dada.auricchio@gmail.com>
2015-12-12 15:40:37 +0100

    upd

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     #assegnando una view nominata post_list alla URL ^$
     #il nome dell'URL che verrà usata per identificare la view.
+
+
+     url(r'^post/(?P<pk>[0-9]+)/(?P<a>\w+)/$', views.post_detail, name='post_detail2'),
 ]
